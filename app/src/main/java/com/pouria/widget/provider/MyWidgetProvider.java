1d7499a172967738e67fdec90fe07bc3224bef33
Pouria Faraji <pouria.faraji@gmail.com>
2018-02-21 14:39:36 +0330

    skull

---
+++ app/src/main/java/com/pouria/widget/provider/MyWidgetProvider.java
@@ -0,0 +1,16 @@
+package com.pouria.widget.provider;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by p.faraji on 2/21/2018.
+ */
+
+public class MyWidgetProvider extends AppWidgetProvider{
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
